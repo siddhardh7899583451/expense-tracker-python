7039e554e76d6a5d7b30f9fdcb03f1f74e6e9774
siddhardh kuncha <siddhardh5030616@gmail.com>
2026-07-25 22:34:15 +0530

    fix: create CSV header for empty file

---
+++ expense_tracker/storage.py
@@ -0,0 +1,53 @@
+import csv
+from pathlib import Path
+from expense_tracker.models import Expense
+
+
+class CSVStorage:
+    def __init__(self, filename="data/expenses.csv"):
+        self.file = Path(filename)
+
+        # Create the data folder if it doesn't exist
+        self.file.parent.mkdir(parents=True, exist_ok=True)
+
+        # Create CSV file with header if it doesn't exist
+        if not self.file.exists() or self.file.stat().st_size == 0:
+            with open(self.file, "w", newline="", encoding="utf-8") as f:
+                writer = csv.writer(f)
+                writer.writerow([
+                    "expense_id",
+                    "title",
+                    "amount",
+                    "category",
+                    "date"
+                ])
+
+    def save(self, expense: Expense):
+        with open(self.file, "a", newline="", encoding="utf-8") as f:
+            writer = csv.writer(f)
+            writer.writerow([
+                expense.expense_id,
+                expense.title,
+                expense.amount,
+                expense.category,
+                expense.date
+            ])
+
+    def load(self):
+        expenses = []
+
+        with open(self.file, "r", newline="", encoding="utf-8") as f:
+            reader = csv.DictReader(f)
+
+            for row in reader:
+                expenses.append(
+                    Expense(
+                        title=row["title"],
+                        amount=float(row["amount"]),
+                        category=row["category"],
+                        date=row["date"],
+                        expense_id=row["expense_id"],
+                    )
+                )
+
+        return expenses
--- main.py
+++ main.py
@@ -1,15 +1,26 @@
 from expense_tracker.models import Expense
+from expense_tracker.storage import CSVStorage
 
 
 def main():
+    storage = CSVStorage()
+
     expense = Expense(
         title="Lunch",
-        amount=250.0,
-        category="Food"
+        amount=250,
+        category="Food",
     )
 
-    print("Expense created successfully!")
-    print(expense)
+    storage.save(expense)
+
+    print("Expense saved successfully!\n")
+
+    print("All Expenses:\n")
+
+    expenses = storage.load()
+
+    for expense in expenses:
+        print(expense)
 
 
 if __name__ == "__main__":
