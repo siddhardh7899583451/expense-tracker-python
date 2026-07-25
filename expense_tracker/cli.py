a3bcd100ac472db27ed79d6b1438ddd347f78289
siddhardh kuncha <siddhardh5030616@gmail.com>
2026-07-25 23:16:24 +0530

    feat: implement add and view expenses

--- expense_tracker/cli.py
+++ expense_tracker/cli.py
@@ -1,4 +1,9 @@
+from datetime import date
+
 from expense_tracker.constants import MENU
+from expense_tracker.manager import ExpenseManager
+
+manager = ExpenseManager()
 
 
 def run():
@@ -8,10 +13,40 @@ def run():
         choice = input("Enter your choice: ").strip()
 
         if choice == "1":
-            print("\nAdd Expense selected.\n")
+            print("\nAdd Expense\n")
+
+            title = input("Title: ").strip()
+            amount = float(input("Amount: "))
+            category = input("Category: ").strip()
+            expense_date = input(
+                "Date (YYYY-MM-DD, leave blank for today): "
+            ).strip()
+
+            if not expense_date:
+                expense_date = str(date.today())
+
+            manager.add_expense(
+                title,
+                amount,
+                category,
+                expense_date,
+            )
+
+            print("\nExpense added successfully!\n")
 
         elif choice == "2":
-            print("\nView Expenses selected.\n")
+            expenses = manager.get_all_expenses()
+
+            print()
+
+            if not expenses:
+                print("No expenses found.\n")
+                continue
+
+            for expense in expenses:
+                print(expense)
+
+            print()
 
         elif choice == "3":
             print("\nSearch Expenses selected.\n")
